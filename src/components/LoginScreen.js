d6648df065d3056c65bd337c28feb5feca916fb0
Leonardo Tiago <pequenotiago@gmail.com>
2024-08-04 19:25:42 +0100

    Login interface improvements

--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 // src/components/LoginScreen.js
 import React, { useState } from 'react';
 import './LoginScreen.css';
@@ -13,20 +14,26 @@ function LoginScreen({ onLogin }) {
 
   return (
     <div className="login-screen">
-      <h2>Login</h2>
-      <input
-        type="text"
-        placeholder="Username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={handleLogin}>Login</button>
+     <div className="login-form">
+        <h2>Login</h2>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <a href="#" className="forgot-password">Esqueceu a sua senha?</a>
+        <button onClick={handleLogin}>Entrar</button>
+      </div>
+      <div className="login-image">
+      <img src={`${process.env.PUBLIC_URL}/gestao-empresarial.jpg`} alt="gestao-empresarial" className="gestao-empresarial" />
+      </div>
     </div>
   );
 }
